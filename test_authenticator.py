df5c0f38f09f57699a148408b10dd3d6ca75df60
Albert-Jonas <60885352+Albert-Jonas@users.noreply.github.com>
2023-06-06 19:10:24 +0200

    Hibás állapot

--- test_authenticator.py
+++ test_authenticator.py
@@ -1,11 +1,21 @@
 import pytest
 from authenticator import *
 
+class DatabaseHandlerTest(IDatabaseHandler):
+    def get_user_credentials(userName):
+        return "Almafa"
+
+
+
 def testAuthenticator():
     auth = Authenticator()
     result = auth.authenticate("Jóska","Almafa")
     assert result == True
 
 
+def testAuthenticator():
+    databaseHandler = DatabaseHandlerTest()
+    auth = AuthenticatorTestable(databaseHandler)
 
-
+    result = auth.authenticate("Jóska","Almafa")
+    assert result == True
